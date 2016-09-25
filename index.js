d5bd9cf4dbeb26b798c6fed3cdde400d1f108a95
Joe Kale <joe.kale1217@gmail.com>
2016-09-25 16:09:40 -0500

    adds basic functionality

--- index.js
+++ index.js
@@ -2,5 +2,52 @@
  * Created by joekale on 9/24/16.
  */
 'use strict';
-const AWS = require("aws-sdk");
-const dynamodb = AWS.dynamoD
+const AWS = require('aws-sdk');
+const dynamodb = AWS.DynamoDB({apiVersion: '2012-08-10'});
+const TABLE = "Meals";
+const SINGLE = "SINGLE";
+const DOUBLE = "DOUBLE";
+const LONG = "LONG";
+/**
+ * The following JSON template shows what is sent as the payload:
+ {
+     "serialNumber": "GXXXXXXXXXXXXXXXXX",
+     "batteryVoltage": "xxmV",
+     "clickType": "SINGLE" | "DOUBLE" | "LONG"
+ }
+ *
+ * A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
+ * "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
+ *
+ * For more documentation, follow the link below.
+ * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
+ */
+exports.handler = (event, context, callback) => {
+    var mealResult;
+    switch (event.clickType){
+        case SINGLE:
+            mealResult = "ATE";
+            break;
+        case DOUBLE:
+            mealResult = "DELAYED";
+            break;
+        case LONG:
+            mealResult = "SKIPPED";
+            break;
+        default:
+            console.error("clickType did not match expected values: ${event.clickType}");
+    }
+    dynamodb.putItem({
+        "TableName": TABLE,
+        "Item": {
+            "Timestamp": {"S", new Date().toISOString()},
+            "result": {"S", mealResult}
+        }
+    }, function(err, data){
+        if(err){
+            context.done('error', "save failure " + err);
+        }else {
+            context.done('success')
+        }
+    });
+};
